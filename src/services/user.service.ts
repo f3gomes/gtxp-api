b4605c06f8ae5b709d72aa7136af5dc279e4901a
f3gomes <fgomesdeluna@gmail.com>
2024-12-06 15:59:53 -0300

    feat: include hashed pass

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import { User } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
@@ -7,8 +8,12 @@ const createUser = async (data: User): Promise<any> => {
   const userLength = (await prisma.user.count()) + 1;
   const startId = userLength.toString().padStart(5, "0");
 
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(data.password, salt);
+
   const updatedData = {
     ...data,
+    password: hashedPassword,
     pmiId: "CBGPL25" + startId,
   };
 
