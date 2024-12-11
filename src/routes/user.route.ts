14984c8202627156f95de4c2ab4cc3fb26292505
f3gomes <fgomesdeluna@gmail.com>
2024-12-11 08:36:00 -0300

    feat: include user schema validations

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import middleware from "../middlewares/auth";
 import userController from "../controllers/user.controller";
+import { userSchema } from "../schemas/userSchema";
+import { validateData } from "../middlewares/validation";
 
 export const userRouter = express.Router();
 
-userRouter.post("/user/new", userController.postUser);
+userRouter.post("/user/new", validateData(userSchema), userController.postUser);
 userRouter.get("/user/list", middleware.auth, userController.getUsers);
 userRouter.post("/login", userController.login);
