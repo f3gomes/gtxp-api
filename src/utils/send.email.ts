c1f1e918a4eb019d7b6bab3087e5e6bcdb9e3c5b
f3gomes <fgomesdeluna@gmail.com>
2024-12-19 14:47:07 -0300

    feat: include nodemailer config

---
+++ src/utils/send.email.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+interface MailProps {
+  email: string;
+  subject: string;
+  text: string;
+}
+
+export const sendMail = ({ email, subject, text }: MailProps) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: Boolean(process.env.SECURE),
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_KEY,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.USER,
+    to: email,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions, function (error: any, info: any) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email enviado: " + info.response);
+    }
+  });
+};
