acd43ed57388fc0795fdcce1080458962ed8fb62
f3gomes <fgomesdeluna@gmail.com>
2025-03-03 11:52:18 -0300

    feat: update post schema

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 export const postSchema = z.object({
-  userId: z.string(),
+  name: z.string(),
+  email: z.string().email(),
   message: z.string(),
   imgUrl: z.string().optional(),
   tags: z.array(z.string()).min(1, {}),
